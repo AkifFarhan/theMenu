22e794435287cb684ca0898989cc0c7471e45f22
Md Akif Farhan <mdakiffarhan@gmail.com>
2026-04-10 15:35:44 +0600

    fix: accept username field in registration

--- server/app/Http/Controllers/Auth/RegisteredUserController.php
+++ server/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -23,13 +23,13 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'name' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
